0089394cafe55d8e63a868b6f76724942fdf712e
rahasibulan <irarismawati687@gmail.com>
2024-12-07 09:50:38 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+     
+     function menampilkan(){
+        echo "hello php";
+     }
+
+     function pergi(){
+        echo "\n saya pamit\n";
+     }
+
+     menampilkan();
+     pergi();
+
+     function menjumlahkan ($angka1, $angka2){
+        return $hasil=$angka1+$angka2;
+
+     }
+
+     menjumlahkan(3,4);
